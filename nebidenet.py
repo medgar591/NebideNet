21cff5832df54fe599737c1215dcf181c4ee4854
Matt Edgar <matthewaedgar@gmail.com>
2021-02-11 01:24:21 -0600

    reorganized net to work on batches

--- nebidenet.py
+++ nebidenet.py
@@ -4,32 +4,19 @@ import torch.nn.functional as F
 import ramogen
 
 # Parameters
-learning_rate = 0.005
+learning_rate = 0.002
 criterion = nn.MSELoss()
 trainingFile = "slrmodels.csv"
 testingFile = "slrTestModels.csv"
 createNewTrainingModels = False
 createNewTestingModels = False
 
-# Define Neural Net
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        # TODO: Check to see if a better structure exists
-        self.l1 = nn.Linear(100, 100)
-        self.l2 = nn.Linear(100, 50)
-        self.l3 = nn.Linear(50, 1)
-        self.dl = nn.Dropout()
-    
-    def forward(self, x):
-        # TODO: Check here too
-        x = F.relu(self.l1(x))
-        x = self.dl(x)
-        x = F.relu(self.l2(x))
-        x = self.l3(x)
-        return x
-
-net = Net()
+# Basic set of linear layers in sequence for the net
+net = nn.Sequential(
+    nn.Linear(100,50),
+    nn.Linear(50, 50),
+    nn.Linear(50, 1)
+)
 
 # Create new nets for training and testing
 if createNewTrainingModels:
@@ -54,6 +41,8 @@ tempLinearBiases = [row.pop(len(row)-1) for row in tempLinearNets]
 linearNets = torch.tensor(tempLinearNets, dtype=torch.float)
 linearBiases = torch.tensor(tempLinearBiases, dtype=torch.float)
 
+n = 0
+
 tolerance = 0.025
 correct = 0
 total = 0
@@ -68,20 +57,23 @@ print("Accuracy of the network on before training: %f %%" % (100.0*correct/total
 
 #Training loop
 print("Beginning Training")
-for epoch in range(10):
+for epoch in range(5):
     running_loss = 0.0
-    for n in range (len(linearNets)):
+    for n in range (0, len(linearNets), 5):
+        output = net(linearNets[n:(n+5)])#.squeeze() # TODO: find out why squeezing this to remove the warning reduces accuracy by 1/3
         net.zero_grad()
-        output = net(linearNets[n])
-        loss = criterion(output, torch.tensor([linearBiases[n]])) # TODO: find out why just doing linearBiases[n] suddenly broke
+        loss = criterion(output, linearBiases[n:(n+5)])
         loss.backward()
-        for f in net.parameters():
-            f.data.sub_(f.grad.data * learning_rate)
+        with torch.no_grad():
+            for param in net.parameters():
+                param -= param.grad * learning_rate
+        #for f in net.parameters():
+        #    f.data.sub_(f.grad.data * learning_rate)
         
         running_loss += loss.item()
-        if n % 500 == 499:    # print every 500 mini-batches
+        if n % 500 == 495:    # print every 500 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, n + 1, running_loss / 500))
+                  (epoch + 1, n + 5, running_loss / 500))
             running_loss = 0.0
 
 print('Finished Training')
