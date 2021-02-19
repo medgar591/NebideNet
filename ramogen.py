6dcf42beb94063c34438459e4412394e987f54b6
Matt Edgar <matthewaedgar@gmail.com>
2021-02-18 18:03:20 -0600

    Added function for creating statistical data of models

--- ramogen.py
+++ ramogen.py
@@ -4,6 +4,7 @@
 # Verson 0.4
 
 import numpy
+import statistics
 
 # Generates a number of Simple Linear Regression models
 # coefficients determines the number of coefficients of the models
@@ -108,5 +109,104 @@ def testSLR(modelFile, dataFile, ignore: list, sensitive):
     files.writelines(models)
     files.close()
 
+# Calculates various statistics about the provided models and outputs them
+def statSLR(modelFile, dataFile, outputFile, ignore: list, sensitive, header=True):
+    files = open(modelFile, "rt")
+    models = [item.split(",") for item in files.read().splitlines()]
+    files.close()
+
+    models[:] = [ [float(num) for num in item] for item in models] #Turning strings into usable floats
+
+    files = open(dataFile, "rt")
+    data = [item.split(",") for item in files.read().splitlines()]
+    files.close()
+
+    data[:] = [ [float(num) for num in item] for item in data] # NOTE: This treats every data point as a float, even when some are binary
+
+    # Delete ignored columns that are greater than the sensitive column
+    ignore.sort(reverse=True) # When columns are removed from highest index to lowest, they don't mess with the next column deletion
+    for column in [item > sensitive for item in ignore]: 
+        for row in data:
+            del row[column]
+    
+    # Pop sensitive column and convert to boolean
+    sa = [bool(row.pop(sensitive)) for row in data]
+
+    # Delete the rest of the columns
+    for column in [item < sensitive for item in ignore]:
+        for row in data:
+            del row[column]
+    
+    # Setting up the array to hold the statistics
+    numStats = 7
+    # Declared here, header to be added later
+    stats = [[0 for i in range(numStats)] for j in range(len(models))]
+    # stats = [len(models)][numStats]
+    #if header:
+    #    stats[len(models)+1][numStats]
+    #    stats[0] = ["PgS", "PgNS", "Parity", "Ratio", "Median", "MeanScore", "DeviationScore"]
+    #else:
+    #    stats[len(models)][numStats]
+
+
+    #Scoring models
+    lendata = len(data)
+    for b in range(len(models)):
+        score = [0.0]*lendata
+        for x in range(lendata):
+            score[x] = sum(
+                [models[b][n+1] * data[x][n] for n in range(len(data[x]))],
+                models[b][0])
+            score[x] = max(0.0, min(score[x], 1.0)) # Effectively clamps results to a 0 to 1 scale
+        stats[b][4] = statistics.median(score)
+        stats[b][5] = statistics.mean(score)
+        stats[b][6] = statistics.stdev(score)
+        
+        #Calculating Statistical Parity
+        st = sa.count(True) #Number of cases where Sensitive is True
+        sf = len(sa) - st #Number of cases where Sensitive is False
+        stt = 0.0 #Number of cases where Sensitive is True and score is True
+        sft = 0.0 #Number of cases where Sensitive is False and score is True
+
+        for n in range(len(sa)):
+            if score[n] >= 0.5:
+                if sa[n]:
+                    stt += 1
+                else:
+                    sft += 1
+
+        """
+        parity = 0
+        if (sft == 0):
+            if (stt == 0):
+                parity = 1
+            else:
+                parity = 2 # TODO: Find the appropriate weight for accepting sensitive while ignoring non-sensitive
+        else: # TODO: Consider the problem of a extremely small acceptance rate that achieved 0 for minorities
+            parity = (stt/st) / (sft/sf) #Parity ratio = P(score+ | sensitive+) / P(score+ | sensitive-)
+        """
+        stats[b][0] = stt/st
+        stats[b][1] = sft/sf
+        stats[b][2] = (stt/st) - (sft/sf)
+        
+        if (stt == 0 or sft == 0):
+            stats[b][3] = -1
+        else:
+            stats[b][3] = (stt/st) / (sft/sf)
+    
+    #Writing stats to the ouput
+    if header:
+        stats.insert(0, ["PgS", "PgNS", "Parity", "Ratio", "Median", "MeanScore", "DeviationScore"])
+    for item in range(len(stats)):
+        stats[item] = ",".join(map(str, stats[item]))
+        stats[item] += "\n"
+    files = open(outputFile, "wt")
+    files.writelines(stats)
+    files.close()
+
+
 # genSLR(100,1000,"slrmodels.csv") #Generates 1,000 sample models to test on the communitycrime dataset
 # testSLR("slrmodels.csv", "communitycrime/crimecommunity.csv", [100, 101], 0)
+
+genSLR(100, 1000, "testStats.csv")
+statSLR("testStats.csv", "communitycrime/crimecommunity.csv", "testStats.csv", [100, 101], 0)
